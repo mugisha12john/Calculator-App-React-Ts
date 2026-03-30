207be85e5f1b35484f55a8fbf9b51265e6065884
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-03-30 17:51:13 +0200

    feat: create components folder and make button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import Buttons from "./components/Buttons";
+
 function App() {
   return (
     <>
-      <h1>Calculator</h1>
-        <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <main className="flex items-center justify-center h-screen">
+        <section>
+          <div className="bg-gray-600 w-[26rem] text-white font-bold text-4xl text-right p-2">
+            90
+          </div>
+          <Buttons/>
+        </section>
+      </main>
     </>
   );
 }
---
+++ src/components/Buttons.tsx
@@ -0,0 +1,21 @@
+
+export default function Buttons() {
+  return (
+    <>
+      <div className="flex">
+        <button className="p-10 flex justify-center items-center font-semibold text-4xl border-2 border-gray-400 ">
+          7
+        </button>
+        <button className="p-10 flex justify-center items-center font-semibold text-4xl border-2 border-gray-400 ">
+          8
+        </button>
+        <button className="p-10 flex justify-center items-center font-semibold text-4xl border-2 border-gray-400 ">
+          9
+        </button>
+        <button className="p-10 flex justify-center items-center font-semibold text-4xl border-2 border-gray-400 ">
+          X
+        </button>
+      </div>
+    </>
+  );
+}
